be4a98cd69797ecb2166d6223a4507b067cda3a5
mohammad alsaadi <moalsaadiformal@gmail.com>
2024-04-20 11:14:40 +0300

    server is done locally !

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
 app.post("/create-checkout-session", async (req, res) => {
   const { items, discount } = req.body;
@@ -36,7 +36,7 @@ app.post("/create-checkout-session", async (req, res) => {
       };
     });
 
-    // Add shipping line item to the lineItems array
+    // Add shipping
     if (totalPrice < 70) {
       lineItems.push({
         price_data: {
@@ -44,7 +44,7 @@ app.post("/create-checkout-session", async (req, res) => {
           product_data: {
             name: "Shipping",
           },
-          unit_amount: 3000, // Assuming shipping cost is 3 JOD
+          unit_amount: 3000, //  shipping cost is 3 JOD
         },
         quantity: 1,
       });
@@ -53,7 +53,7 @@ app.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       mode: "payment",
 
-      line_items: lineItems, // Use the lineItems array when creating the session
+      line_items: lineItems,
       success_url: `${process.env.CLIENT_URL}/success-payment`,
       cancel_url: `${process.env.CLIENT_URL}/checkout`,
     });
@@ -67,63 +67,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-/*
-require("dotenv").config();
-
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
-
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
-
-const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Whey Protein" }],
-  [2, { priceInCents: 20000, name: "Iso Protein" }],
-]);
-
-app.post("/create-checkout-session", async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: req.body.items.map((item) => {
-        const storeItem = storeItems.get(item.id);
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: storeItem.name,
-            },
-            unit_amount: storeItem.priceInCents,
-          },
-          quantity: item.quantity,
-        };
-        //   price_data: {
-        //     currency: "JOD", // Change to JD
-        //     product_data: {
-        //       name: "Total Price",
-        //     },
-        //     unit_amount: totalPrice * 100,
-        //   },
-        //   quantity: 1,
-        // };
-      }),
-      success_url: `${process.env.CLIENT_URL}/success-payment`,
-      cancel_url: `${process.env.CLIENT_URL}/checkout`,
-    });
-    res.json({ url: session.url });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-*/
