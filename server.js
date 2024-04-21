f22b2cc2b78a866727a410bb2cdffa1d944acf0c
mohammad alsaadi <moalsaadiformal@gmail.com>
2024-04-22 00:11:06 +0300

    server11

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
